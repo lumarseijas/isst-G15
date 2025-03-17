b7d336877f5f5846e978eba09e8e50466b7bddfa
Cristina <crl311202@gmail.com>
2025-03-17 19:59:10 +0100

    modified:   reserva-belleza-web/src/pages/Admin.jsx

--- reserva-belleza-web/src/pages/Admin.jsx
+++ reserva-belleza-web/src/pages/Admin.jsx
@@ -2,7 +2,7 @@ const Admin = () => {
     return (
       <div>
         <h2>Panel de Administración</h2>
-        <p>gestionar reservas, servicios y trabajadores</p>
+        <p>gestionar reservas, servicios y trabajadores.</p>
       </div>
     );
   };
