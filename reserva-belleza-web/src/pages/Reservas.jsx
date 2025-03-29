7649809d8f812d07802f6901600c968e90ae35e9
lumarseijas <lucia.mseijas@alumnos.upm.es>
2025-03-29 14:37:49 +0100

    modified:   reserva-belleza-web/src/pages/Reservas.jsx

--- reserva-belleza-web/src/pages/Reservas.jsx
+++ reserva-belleza-web/src/pages/Reservas.jsx
@@ -2,8 +2,6 @@ import FormularioReserva from "../components/FormularioReserva";
 const Reservas = () => {
     return (
       <div>
-        <h2>Reservar una Cita</h2>
-        <p>Elige un servicio y un horario disponible.</p>
         <FormularioReserva/>
       </div>
     );
